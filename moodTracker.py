9cde37440d1b415665b963a54491796763c5db73
rbenesova <renata.benesova@infis.cz>
2025-06-03 09:20:40 +0200

    Add files via upload

---
+++ moodTracker.py
@@ -0,0 +1,13 @@
+mood = input("Ahoj, jak se dnes cítís?? (stastne/smutne/ve stresu/unavene/nastvane)")
+if mood =="stastne":
+    print("to je super ze se dnes citis stastne, hlavne si dnesek uzij")
+elif mood =="smutne":
+    print("To je v poradku, ne kazdy den se dari. Udelej si cas na sebe a zkus si to uzit")
+elif mood == "ve stresu":
+    print("to me mrzi, zkus se na chvíli zastavit a poradne dychat, Hlavní je ze delas to co muzes a to staci")
+elif mood == "unavene":
+    print("to je proto ze si tvoje telo rika o pauzu, hlavne si odpocin")
+elif mood == "nastvane":
+    print("to je uplne v poradku, vztek je uplne noramalni emoce, Zkus najít neco co te dokaze uklidnit")
+else:
+    print("DEKUJI za to ze si dnes sdilel jak se dnes citis doufam ze si zbytek dne uzijes a budes mit dobry den")
